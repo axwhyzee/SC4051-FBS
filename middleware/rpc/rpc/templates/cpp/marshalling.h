d9805058e3acd67e7525ce438653b37f0f5fe620
Siah Wee Hung <siah_weehung@yahoo.com>
2025-03-30 19:05:17 +0800

    add docstring for marshalling

--- middleware/rpc/rpc/templates/cpp/marshalling.h
+++ middleware/rpc/rpc/templates/cpp/marshalling.h
@@ -2,6 +2,26 @@
 
 #pragma once
 
+
+/**
+ * Marshall structs and primitives into byte stream.
+ * 
+ * The diagram below shows the structure of an RPC message. Numbers in
+ * parenthesis represent length of field in bytes.
+ * 
+ * 
+ * +------------------+---------------+------------------+-----------------+
+ * |  MESSAGE_ID (1)  |  ARG1_ID (1)  |  ARG1_LEN (0/4)  |  ARG_1_VAL (N)  |
+ * +------------------+---------------+------------------+-----------------+ 
+ *                    |           ... repeat for other args ...            |
+ *                    +---------------+------------------+-----------------+
+ * 
+ * - Structs are flattened according to the order by which the attributes are
+ *   defined in the interface file.
+ * - Only variable-length types like strings and sequences have arg_len 
+ *   headers.
+ */
+
 void _marshall_len_header(char* message, int& i, int len) {
     int32_t network_val = htonl(len);
     std::memcpy(&message[i], &network_val, sizeof(int32_t));
