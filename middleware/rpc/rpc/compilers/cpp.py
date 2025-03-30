d394b9ce977fc4d62ccca4adce6de6779f5f1987
Siah Wee Hung <siah_weehung@yahoo.com>
2025-03-30 22:23:50 +0800

    avoid var name conflicts

--- middleware/rpc/rpc/compilers/cpp.py
+++ middleware/rpc/rpc/compilers/cpp.py
@@ -24,7 +24,7 @@ from pathlib import Path
 from typing import Dict
 
 from .base import BaseCompiler
-from .common import translate_attr, translate_attr_type
+from .common import translate_attr, translate_attr_type, is_sequence, get_nested_type
 from .model import EnumModel, InterfaceModel, StructModel
 from .typings import DType
 
@@ -48,18 +48,10 @@ _translate_attr = partial(translate_attr, dtypes=CPP_DTYPES)
 _translate_attr_type = partial(translate_attr_type, dtypes=CPP_DTYPES)
 
 
-def _get_nested_type(attr_type: str):
-    return attr_type.rstrip(">").split("<", 1)[1]
-
-
-def _is_sequence(attr_type: str):
-    return attr_type.startswith(DType.SEQUENCE.value)
-
-
 class CPPCompiler(BaseCompiler):
     @classmethod
     def _handle_struct(
-        cls, model: StructModel, out_dir: Path, out_dir_relative_to: Path
+        cls, model: StructModel, out_dir: Path, root_dir: Path
     ) -> None:
         """
         Example:
@@ -99,25 +91,25 @@ class CPPCompiler(BaseCompiler):
 
         def create_unmarshalling():
             code = f"{model.name} unmarshall_{model.name}(char* message, int& i) {{\n"
-            code += f"\t{model.name} {model.name}_struct;\n"
+            code += f"\t{model.name} {model.name}__struct;\n"
 
             for attr in model.attrs:
                 attr_type = attr.type
                 translated_attr_type = _translate_attr_type(attr.type)
 
-                if _is_sequence(attr_type):
+                if is_sequence(attr_type):
                     # iteratively marshall sequence items
-                    nested_type = _get_nested_type(attr_type)
-                    code += f"\tint {attr.name}_len = unmarshall_int(message, i);\n"
-                    code += f"\t{translated_attr_type} temp_{attr.name} = {translated_attr_type}();\n"
-                    code += f"\tfor (int j=0; j<{attr.name}_len; j++)\n"
-                    code += f"\t\ttemp_{attr.name}.push_back(unmarshall_{nested_type}(message, i));\n"
-                    code += f"\t{model.name}_struct.{attr.name} = temp_{attr.name};\n"
+                    nested_type = get_nested_type(attr_type)
+                    code += f"\tint {attr.name}__len = unmarshall_int(message, i);\n"
+                    code += f"\t{translated_attr_type} temp__{attr.name} = {translated_attr_type}();\n"
+                    code += f"\tfor (int j=0; j<{attr.name}__len; j++)\n"
+                    code += f"\t\ttemp__{attr.name}.push_back(unmarshall_{nested_type}(message, i));\n"
+                    code += f"\t{model.name}__struct.{attr.name} = temp__{attr.name};\n"
                 else:
                     # structs and fixed-length primitives
-                    code += f"\t{model.name}_struct.{attr.name} = unmarshall_{attr_type}(message, i);\n"
+                    code += f"\t{model.name}__struct.{attr.name} = unmarshall_{attr_type}(message, i);\n"
 
-            code += f"\treturn {model.name}_struct;\n"
+            code += f"\treturn {model.name}__struct;\n"
             code += "}\n\n"
             with open(out_dir / UNMARSHALLING_FILE, "a") as f:
                 f.write(code)
@@ -125,9 +117,9 @@ class CPPCompiler(BaseCompiler):
         def create_marshalling():
             code = f"{model.name} marshall_{model.name}(char* message, int& i, {model.name} val) {{\n"
             for attr in model.attrs:
-                if _is_sequence(attr.type):
+                if is_sequence(attr.type):
                     # sequences
-                    nested_type = _get_nested_type(attr.type)
+                    nested_type = get_nested_type(attr.type)
                     code += f"\t_marshall_len_header(message, i, val.{attr.name}.size());\n"
                     code += f"\tfor (int j=0; j<val.{attr.name}.size(); j++)\n"
                     code += f"\t\tmarshall_{nested_type}(message, i, val.{attr.name}[j]);\n"
@@ -146,7 +138,7 @@ class CPPCompiler(BaseCompiler):
 
     @classmethod
     def _handle_enum(
-        cls, model: EnumModel, out_dir: Path, out_dir_relative_to: Path
+        cls, model: EnumModel, out_dir: Path, root_dir: Path
     ) -> None:
         """
         Example:
@@ -201,7 +193,7 @@ class CPPCompiler(BaseCompiler):
 
     @classmethod
     def _handle_interface(
-        cls, model: InterfaceModel, out_dir: Path, out_dir_relative_to: Path
+        cls, model: InterfaceModel, out_dir: Path, root_dir: Path
     ) -> None:
         def create_service_stub() -> str:
             """
@@ -254,12 +246,12 @@ class CPPCompiler(BaseCompiler):
                 for arg in method.args:
                     arg_name = f"{arg.name}_arg"
                     translated_arg_type = _translate_attr_type(arg.type)
-                    if _is_sequence(arg.type):
-                        nested_type = _get_nested_type(arg.type)
+                    if is_sequence(arg.type):
+                        nested_type = get_nested_type(arg.type)
                         dispatch_code += f"\t\t\t\t{translated_arg_type} {arg_name} = {translated_arg_type}();\n"
-                        dispatch_code += f"\t\t\t\tint {arg_name}_len = unmarshall_int(message, i);\n"
+                        dispatch_code += f"\t\t\t\tint {arg_name}__len = unmarshall_int(message, i);\n"
                         dispatch_code += (
-                            f"\t\t\t\tfor (int j=0; j<{arg_name}_len; j++)\n"
+                            f"\t\t\t\tfor (int j=0; j<{arg_name}__len; j++)\n"
                         )
                         dispatch_code += f"\t\t\t\t\t{arg_name}.push_back(unmarshall_{nested_type}(message, i));\n"
                     else:
@@ -288,11 +280,9 @@ class CPPCompiler(BaseCompiler):
         create_servicer()
 
     @classmethod
-    def compile(
-        cls, in_file: Path, out_dir: Path, out_dir_relative_to: Path
-    ) -> None:
+    def compile(cls, in_file: Path, out_dir: Path, root_dir: Path) -> None:
         # copy templates
         for file in Path("templates/cpp").iterdir():
             if file.suffix in (".h", ".cpp") and not file.stem.startswith("_"):
                 (out_dir / file.name).write_text(file.read_text())
-        super().compile(in_file, out_dir, out_dir_relative_to)
+        super().compile(in_file, out_dir, root_dir)
