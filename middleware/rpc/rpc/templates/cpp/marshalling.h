452f755c0870ab1890eb60d409c5f9b2d33825ae
Siah Wee Hung <siah_weehung@yahoo.com>
2025-03-30 21:02:42 +0800

    update marshalling docstring

--- middleware/rpc/rpc/templates/cpp/marshalling.h
+++ middleware/rpc/rpc/templates/cpp/marshalling.h
@@ -4,22 +4,22 @@
 
 
 /**
- * Marshall structs and primitives into byte stream.
+ * Marshall messages, structs and primitives into byte stream.
  * 
  * The diagram below shows the structure of an RPC message. Numbers in
  * parenthesis represent length of field in bytes.
  * 
- * 
- * +------------------+------------------+----------------+
- * |  MESSAGE_ID (4)  |  ARG1_LEN (0|4)  |  ARG1_VAL (N)  |
- * +------------------+------------------+----------------+
- *                    |    ... repeat for other args ...  |
- *                    +------------------+----------------+
+ * +-----------------+------------------+----------------+
+ * |  METHOD_ID (4)  |  ARG1_LEN (0|4)  |  ARG1_VAL (N)  |
+ * +-----------------+------------------+----------------+
+ *                   |    ... repeat for other args ...  |
+ *                   +------------------+----------------+
  * 
  * - Structs are flattened according to the order by which the attributes are
  *   defined in the interface file.
- * - Only variable-length types like strings and sequences have arg_len 
+ * - Only variable-length types like strings and sequences have ARG_LEN 
  *   headers.
+ * - Responses use the same METHOD_ID as that in the request.
  */
 
 void _marshall_len_header(char* message, int& i, int len) {
