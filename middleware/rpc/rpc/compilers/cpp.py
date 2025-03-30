4ab1093956b091a81c0f202ff7ad20dfd2cdc9ff
Siah Wee Hung <siah_weehung@yahoo.com>
2025-03-30 23:34:53 +0800

    move compiler docstrings

--- middleware/rpc/rpc/compilers/cpp.py
+++ middleware/rpc/rpc/compilers/cpp.py
@@ -1,24 +1,3 @@
-"""
-Marshall messages, structs and primitives into byte stream.
-
-The diagram below shows the structure of an RPC message. Numbers in
-parenthesis represent length of field in bytes.
-
-```
-+-----------------+------------------+----------------+
-|  METHOD_ID (4)  |  ARG1_LEN (0|4)  |  ARG1_VAL (N)  |
-+-----------------+------------------+----------------+
-                  |    ... repeat for other args ...  |
-                  +------------------+----------------+
-```
-
-- Structs are flattened according to the order by which the attributes
-  are defined in the interface file.
-- Only variable-length types like strings and sequences have ARG_LEN
-  headers.
-- Responses use the same METHOD_ID as that in the request.
-"""
-
 from functools import partial
 from pathlib import Path
 from typing import Dict
