34f966dc8b750b4d665bf7e5e588cfbc8983bb57
Siah Wee Hung <siah_weehung@yahoo.com>
2025-04-01 17:09:57 +0800

    bug fix: cpp marshall int

--- middleware/rpc/rpc/templates/cpp/marshalling.cpp
+++ middleware/rpc/rpc/templates/cpp/marshalling.cpp
@@ -1,7 +1,7 @@
 #include "marshalling.h"
 
 #define INT_SIZE 4
-#define ZEROES 255  // 1111 1111 = 255
+#define ONES 255  // 1111 1111 = 255
 
 
 void marshall_len_header(char* message, int& i, int len) {
@@ -10,7 +10,7 @@ void marshall_len_header(char* message, int& i, int len) {
 
 void marshall_int(char* message, int& i, int val) {
     for (int j=0; j<INT_SIZE; j++)
-        message[i++] = val & (ZEROES << (8 * (INT_SIZE - j - 1)));
+        message[i++] = ONES & (val >> (8 * (INT_SIZE - j - 1)));
 }
 
 void marshall_string(char* message, int& i, std::string val) {
