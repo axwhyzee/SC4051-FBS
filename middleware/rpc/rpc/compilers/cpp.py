dcd2fd7a55dc2c091bc6bc2f9218a535f88b990a
Siah Wee Hung <siah_weehung@yahoo.com>
2025-04-01 09:35:22 +0800

    implement cpp interface concrete classes

--- middleware/rpc/rpc/compilers/cpp.py
+++ middleware/rpc/rpc/compilers/cpp.py
@@ -14,11 +14,17 @@ from .typings import DType
 
 TEMPLATE_DIR = Path("rpc/templates/cpp")
 TYPES_FILE = "proto_types.h"
-STUBS_FILE = "stubs.h"
+
+# files to write to
+STUBS_HEADER_FILE = "stubs.h"
 STUBS_CPP_FILE = "stubs.cpp"
 MARSHALLING_FILE = "marshalling.h"
 UNMARSHALLING_FILE = "unmarshalling.h"
-SERVICER_FILE = "_servicer.cpp"
+
+# templates files to read from
+TEMPLATE_SERVICER_CPP_FILE = "_servicer.cpp"
+TEMPLATE_SERVICER_HEADER_FILE = "_servicer.h"
+
 
 
 CPP_DTYPES: Dict[str, str] = {
@@ -183,7 +189,7 @@ class CPPCompiler(BaseCompiler):
     def _handle_interface(
         cls, model: InterfaceModel, out_dir: Path, root_dir: Path
     ) -> None:
-        def create_service_stub() -> str:
+        def create_service_stub_header() -> str:
             """
             Service stub is implemented by server to handle
             incoming RPCs
@@ -194,7 +200,7 @@ class CPPCompiler(BaseCompiler):
             code += f"\tvirtual ~{model.name}() {{}};\n"
 
             for method in model.methods:
-                code += f"\tvirtual {method.ret_type} "
+                code += f"\tvirtual {_translate_attr_type(method.ret_type)} "
                 code += f"{method.name}("
                 code += ", ".join(
                     [_translate_attr(attr) for attr in method.args]
@@ -203,78 +209,127 @@ class CPPCompiler(BaseCompiler):
             code += "};\n\n"
             return code
 
-        def create_client_stub() -> str:
+        def create_client_stub_header() -> str:
             """
             Stub will be called by client to make RPCs
             """
 
-            code = ""
-            code += f"class {model.name}Stub {{\n"
+            code = f"class {model.name}Stub {{\n"
             code += "public:\n"
-            code += f"\t{model.name}Stub() {{}};\n"
+            code += f"\t{model.name}Stub(sockaddr_in server_addr, Protocol& proto) : server_addr(server_addr), proto(proto) {{}};\n"
             code += f"\t~{model.name}Stub() {{}};\n"
 
             for method in model.methods:
-                code += f"\t{_translate_attr_type(method.ret_type)} "
-                code += f"{method.name}("
+                code += f"\t{_translate_attr_type(method.ret_type)} {method.name}("
                 code += ", ".join(
                     [_translate_attr(attr) for attr in method.args]
                 )
-                code += (
-                    ") {/* TODO: marshall and send to server via UDP */};\n"
-                )
+                code += ");\n"
+            
+            code += "private:\n"
+            code += "\tsockaddr_in server_addr;\n"
+            code += "\tProtocol& proto;\n"
             code += "};\n\n"
             return code
+        
+        def create_service_stub():
+            code = ""
+            for method in model.methods:
+                code += f"{_translate_attr_type(method.ret_type)} {model.name}Stub::{method.name}("
+                code += ", ".join(
+                    [_translate_attr(attr) for attr in method.args]
+                )
+                code += ") {\n"
+                code += "\tint i = 0;\n"
+                code += "\tint buffer_size = proto.get_buffer_size();\n"
+                code += "\tchar response_data[buffer_size];\n"
+                code += "\tchar request_data[buffer_size];\n"
+                code += f"\tmarshall_int(request_data, i, {method.id});\n"
+
+                for arg in method.args:
+                    if is_sequence(arg.type):
+                        nested_type = get_nested_type(arg.type)
+                        code += f"\tfor ({nested_type} {arg.name}__item : {arg.name})\n"
+                        code += f"\t\tmarshall_{nested_type}(request_data, i, {arg.name}__item);\n"
+                    else:
+                        code += f"\tmarshall_{arg.type}(request_data, i, {arg.name});\n"
+                
+                code += "\tint response_len = proto.send(server_addr, request_data, i, response_data);\n"
+                code += "\ti = 0;\n"
+                code += "\tunmarshall_int(response_data, i);  // strip method_id\n"  
+                if is_sequence(method.ret_type):
+                    nested_type = get_nested_type(method.ret_type)
+                    translated_type = _translate_attr_type(method.ret_type)
+                    code += f"\t{translated_type} {method.name}__result = {translated_type}();\n"
+                    code += f"\tint {method.name}__result__len = unmarshall_int(response_data, i);\n"
+                    code += f"\tfor (int j=0; j<{method.name}__result__len; j++)\n"
+                    code += f"\t\t{method.name}__result.push_back(unmarshall_{nested_type}(response_data, i));\n"
+                    code += f"\treturn {method.name}__result;\n"
+                else:
+                    code += f"\treturn unmarshall_{method.ret_type}(response_data, i);\n"
+                code += "}\n\n"
+            return code
+        
+        def create_servicer_header() -> str:
+            return (
+                (TEMPLATE_DIR / TEMPLATE_SERVICER_HEADER_FILE)
+                .read_text()
+                .replace("{__SERVICE_NAME__}", model.name)
+            )
 
         def create_servicer() -> str:
             code = "\n"
-            for i, method in enumerate(model.methods, start=1):
-                code += f"\t\t\tcase {i}: {{\n"
+            for method in model.methods:
+                code += f"\t\tcase {cls.method_counter}: {{\n"
                 translated_ret_type = _translate_attr_type(method.ret_type)
                 arg_names = []
                 for arg in method.args:
-                    arg_name = f"{arg.name}_arg"
+                    arg_name = f"{arg.name}__arg"
                     translated_arg_type = _translate_attr_type(arg.type)
                     if is_sequence(arg.type):
                         nested_type = get_nested_type(arg.type)
-                        code += f"\t\t\t\t{translated_arg_type} {arg_name} = {translated_arg_type}();\n"
-                        code += f"\t\t\t\tint {arg_name}__len = unmarshall_int(message, i);\n"
+                        code += f"\t\t\t{translated_arg_type} {arg_name} = {translated_arg_type}();\n"
+                        code += f"\t\t\tint {arg_name}__len = unmarshall_int(request_data, i);\n"
                         code += (
-                            f"\t\t\t\tfor (int j=0; j<{arg_name}__len; j++)\n"
+                            f"\t\t\tfor (int j=0; j<{arg_name}__len; j++)\n"
                         )
-                        code += f"\t\t\t\t\t{arg_name}.push_back(unmarshall_{nested_type}(message, i));\n"
+                        code += f"\t\t\t\t{arg_name}.push_back(unmarshall_{nested_type}(request_data, i));\n"
                     else:
-                        code += f"\t\t\t\t{translated_arg_type} {arg_name} = unmarshall_{arg.type}(message, i);\n"
+                        code += f"\t\t\t{translated_arg_type} {arg_name} = unmarshall_{arg.type}(request_data, i);\n"
                     arg_names.append(arg_name)
-                code += f'\t\t\t\t{translated_ret_type} {method.name}__result = service.{method.name}({", ".join(arg_names)});\n'
-                code += f"\t\t\t\tmarshall_int(response, j, {cls.method_counter});\n"
+                code += f'\t\t\t{translated_ret_type} {method.name}__result = service.{method.name}({", ".join(arg_names)});\n'
+                code += f"\t\t\tmarshall_int(response_data, j, {cls.method_counter});\n"
                 cls.method_counter += 1
 
                 if is_sequence(method.ret_type):
                     nested_type = get_nested_type(method.ret_type)
-                    code += f"\t\t\t\tmarshall_len_header(response, i, {method.name}__result.size());\n"
-                    code += f"\t\t\t\tfor ({nested_type} result__seq__item : {method.name}__result)\n"
-                    code += f"\t\t\t\t\tmarshall_{nested_type}(response, i, result__seq__item);\n"
+                    code += f"\t\t\tmarshall_len_header(response_data, i, {method.name}__result.size());\n"
+                    code += f"\t\t\tfor ({nested_type} result__seq__item : {method.name}__result)\n"
+                    code += f"\t\t\t\tmarshall_{nested_type}(response_data, i, result__seq__item);\n"
                 else:
-                    code += f"\t\t\t\tmarshall_{method.ret_type}(response, j, {method.name}__result);\n"
-                code += "\t\t\t\treturn j;\n"
-                code += "\t\t\t}\n"
+                    code += f"\t\t\tmarshall_{method.ret_type}(response_data, j, {method.name}__result);\n"
+                code += "\t\t\treturn j;\n"
+                code += "\t\t}\n"
 
-            code += "\t\t\tdefault:\n"
-            code += "\t\t\t\tRAISE;"
+            code += "\t\tdefault:\n"
+            code += '\t\t\tthrow std::runtime_error("Invalid method id" + std::to_string(method_id));'
 
-            with open(out_dir / STUBS_CPP_FILE, "a") as f:
-                f.write(
-                    (TEMPLATE_DIR / SERVICER_FILE)
-                    .read_text()
-                    .replace("{__DISPATCH_CODE__}", code)
-                    .replace("{__SERVICE_NAME__}", model.name)
-                )
+            return (
+                (TEMPLATE_DIR / TEMPLATE_SERVICER_CPP_FILE)
+                .read_text()
+                .replace("{__DISPATCH_CODE__}", code)
+                .replace("{__SERVICE_NAME__}", model.name)
+            )
 
-        with open(out_dir / STUBS_FILE, "a") as f:
+        with open(out_dir / STUBS_HEADER_FILE, "a") as f:
+            f.write(create_service_stub_header())
+            f.write(create_client_stub_header())
+            f.write(create_servicer_header())
+
+        with open(out_dir / STUBS_CPP_FILE, "a") as f:
             f.write(create_service_stub())
-            f.write(create_client_stub())
-        create_servicer()
+            f.write(create_servicer())
+            
 
     @classmethod
     def compile(cls, in_file: Path, out_dir: Path, root_dir: Path) -> None:
