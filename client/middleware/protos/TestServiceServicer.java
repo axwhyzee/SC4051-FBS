81308eb22a781c5e585051e295aacd86d5edfc7b
Qhoebe <116541831+Qhoebe@users.noreply.github.com>
2025-04-02 09:53:01 +0800

    Test Server

---
+++ client/middleware/protos/TestServiceServicer.java
@@ -0,0 +1,42 @@
+package middleware.protos;
+
+import middleware.network.Servicer;
+import middleware.network.Bytes;
+
+
+public class TestServiceServicer implements Servicer {
+    private final TestService service;
+
+    public TestServiceServicer(TestService service) {
+        this.service = service;
+    }
+
+    public int _dispatch(byte[] message, byte[] response) throws Exception {
+        int[] i = new int[]{0};
+        int method_id = Unmarshaller.unmarshall_int(message, i);
+
+        switch (method_id) {
+			case 1:
+				int days__1__arg__len = Unmarshaller.unmarshall_int(message, i);
+				Day[] days__1__arg = new Day[days__1__arg__len];
+				for (int j=0; j<days__1__arg__len; j++)
+					days__1__arg[j] = Unmarshaller.unmarshall_Day(message, i);
+				DayTime[] generate_noon_daytimes__result = service.generate_noon_daytimes(days__1__arg);
+				i[0] = 0;
+				Marshaller.marshall_int(response, i, 1);
+				Marshaller.marshall_len_header(response, i, generate_noon_daytimes__result.length);
+				for (DayTime generate_noon_daytimes__result__item : generate_noon_daytimes__result)
+					Marshaller.marshall_DayTime(response, i, generate_noon_daytimes__result__item);
+				return i[0];
+			default:
+				throw new Exception("Unexpected method ID: " + method_id);
+        }
+    }
+    
+    @Override
+    public Bytes callback(Bytes data, int buffer_size) throws Exception {
+        byte[] response = new byte[buffer_size];
+        int response_len = _dispatch(data.bytes(), response);
+        return new Bytes(response, response_len);
+    }
+}
---
+++ client/middleware/protos/TestServiceStub.java
@@ -0,0 +1,40 @@
+package middleware.protos;
+
+import java.net.InetAddress;
+import middleware.network.Protocol;
+import middleware.network.Bytes;
+
+
+public class TestServiceStub {
+	private final InetAddress server_addr;
+	private final int server_port;
+	private final Protocol proto;
+	
+	public TestServiceStub(InetAddress server_addr, int server_port, Protocol proto) {
+		this.server_addr = server_addr;
+		this.server_port = server_port;
+		this.proto = proto;
+	}
+
+	public Bytes _send(Bytes data) {
+		return proto.send(server_addr, server_port, data);
+	}
+
+	public DayTime[] generate_noon_daytimes(Day[] days) {
+		int[] i = {0};
+		byte[] request_data = new byte[proto.get_buffer_size()];
+		Marshaller.marshall_int(request_data, i, 1);
+		Marshaller.marshall_len_header(request_data, i, days.length);
+		for (Day days__arg : days)
+			Marshaller.marshall_Day(request_data, i, days__arg);
+		byte[] response_data = _send(new Bytes(request_data, i[0])).bytes();
+		i[0] = 0;
+		Unmarshaller.unmarshall_int(response_data, i);  // strip method_id
+		int response__seq__len = Unmarshaller.unmarshall_int(response_data, i);
+		DayTime[] response__seq = new DayTime[response__seq__len];
+		for (int j=0; j<response__seq__len; j++)
+			response__seq[j] = Unmarshaller.unmarshall_DayTime(response_data, i);
+		return response__seq;
+	}
+
+}
