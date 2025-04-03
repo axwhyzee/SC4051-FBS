64bb4526d45354edb3653e996c6738873fd776b9
Siah Wee Hung <siah_weehung@yahoo.com>
2025-04-03 16:05:30 +0800

    bug fix: unmarshall str length twice

--- server/middleware/protos/marshalling.cpp
+++ server/middleware/protos/marshalling.cpp
@@ -36,18 +36,14 @@ void marshall_Interval(char* message, int& i, Interval val) {
 
 void marshall_Booking(char* message, int& i, Booking val) {
 	marshall_int(message, i, val.bookingId);
-	marshall_len_header(message, i, val.user.length());
 	marshall_string(message, i, val.user);
-	marshall_len_header(message, i, val.facilityName.length());
 	marshall_string(message, i, val.facilityName);
 	marshall_DayTime(message, i, val.start);
 	marshall_DayTime(message, i, val.end);
 }
 
 void marshall_Facility(char* message, int& i, Facility val) {
-	marshall_len_header(message, i, val.name.length());
 	marshall_string(message, i, val.name);
-	marshall_len_header(message, i, val.type.length());
 	marshall_string(message, i, val.type);
 	marshall_len_header(message, i, val.bookings.size());
 	for (int j=0; j<val.bookings.size(); j++)
@@ -55,12 +51,10 @@ void marshall_Facility(char* message, int& i, Facility val) {
 }
 
 void marshall_Response(char* message, int& i, Response val) {
-	marshall_len_header(message, i, val.error.length());
 	marshall_string(message, i, val.error);
 }
 
 void marshall_AvailabilityResponse(char* message, int& i, AvailabilityResponse val) {
-	marshall_len_header(message, i, val.error.length());
 	marshall_string(message, i, val.error);
 	marshall_len_header(message, i, val.availability.size());
 	for (int j=0; j<val.availability.size(); j++)
@@ -68,13 +62,11 @@ void marshall_AvailabilityResponse(char* message, int& i, AvailabilityResponse v
 }
 
 void marshall_BookResponse(char* message, int& i, BookResponse val) {
-	marshall_len_header(message, i, val.error.length());
 	marshall_string(message, i, val.error);
 	marshall_int(message, i, val.bookingId);
 }
 
 void marshall_FacilitiesResponse(char* message, int& i, FacilitiesResponse val) {
-	marshall_len_header(message, i, val.error.length());
 	marshall_string(message, i, val.error);
 	marshall_len_header(message, i, val.facilities.size());
 	for (int j=0; j<val.facilities.size(); j++)
